490404333136453cd63c1f57f1184d13cf63e1b4
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-08-29 11:36:36 +0500

    Init Data access

---
+++ src/ParkingZoneMinimalApi/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingZoneMinimalApi.Models;
+
+namespace ParkingZoneMinimalApi.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<ParkingZone> ParkingZones { get; set; }
+        public DbSet<ParkingSlot> ParkingSlots { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ParkingZone>()
+                .HasMany(p => p.ParkingSlots)
+                .WithOne(x => x.ParkingZone)
+                .HasForeignKey(x => x.ParkingZoneId);
+
+            modelBuilder.Entity<ParkingSlot>()
+                .HasMany(r => r.Reservations)
+                .WithOne(x => x.ParkingSlot)
+                .HasForeignKey(x => x.ParkingSlotId);
+        }
+    }
+}
