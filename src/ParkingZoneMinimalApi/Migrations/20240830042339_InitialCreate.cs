ca8dbec95c7f854c8fa23fa6a744e501a3ea4ac3
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-08-30 09:25:00 +0500

    add migrations

---
+++ src/ParkingZoneMinimalApi/Migrations/20240830042339_InitialCreate.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkingZoneMinimalApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParkingZones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreatedDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParkingZones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParkingSlots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SlotNumber = table.Column<short>(type: "smallint", nullable: false),
+                    IsLocked = table.Column<bool>(type: "bit", nullable: false),
+                    ParkingZoneId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParkingSlots", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ParkingSlots_ParkingZones_ParkingZoneId",
+                        column: x => x.ParkingZoneId,
+                        principalTable: "ParkingZones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<short>(type: "smallint", nullable: false),
+                    ParkingSlotId = table.Column<int>(type: "int", nullable: false),
+                    VehicleNo = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_ParkingSlots_ParkingSlotId",
+                        column: x => x.ParkingSlotId,
+                        principalTable: "ParkingSlots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParkingSlots_ParkingZoneId",
+                table: "ParkingSlots",
+                column: "ParkingZoneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_ParkingSlotId",
+                table: "Reservations",
+                column: "ParkingSlotId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "ParkingSlots");
+
+            migrationBuilder.DropTable(
+                name: "ParkingZones");
+        }
+    }
+}
--- src/ParkingZoneMinimalApi/Program.cs
+++ src/ParkingZoneMinimalApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using ParkingZoneMinimalApi.DataAccess;
+
 namespace ParkingZoneMinimalApi
 {
     public static class Program
@@ -11,7 +14,8 @@ namespace ParkingZoneMinimalApi
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
