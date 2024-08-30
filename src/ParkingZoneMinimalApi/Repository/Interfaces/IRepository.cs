4a32b6c1f1baf4f6cb68b128692dabee66605a3c
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-08-30 09:50:45 +0500

    craete IRepository interface

---
+++ src/ParkingZoneMinimalApi/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace ParkingZoneMinimalApi.Repository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAll();
+
+        public Task<T> GetById(int id);
+
+        public Task<T> Create(T entity);
+
+        public Task<T> Update(T entity);
+
+        public Task<bool> Delete(int id);
+    }
+}
---
+++ src/ParkingZoneMinimalApi/Repository/Repository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingZoneMinimalApi.DataAccess;
+using ParkingZoneMinimalApi.Repository.Interfaces;
+
+namespace ParkingZoneMinimalApi.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        
+    }
+}
