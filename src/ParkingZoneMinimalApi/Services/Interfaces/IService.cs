1b0e5bc62631156d0370b417d1aa617a497d3074
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-08-30 10:35:15 +0500

    implement IService interface

---
+++ src/ParkingZoneMinimalApi/Services/Interfaces/IService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ParkingZoneMinimalApi.Services.Interfaces
+{
+    public interface IService<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+
+        public Task<T?> GetByIdAsync(int id);
+
+        public Task CreateAsync(T entity);
+
+        public Task<bool> UpdateAsync(T entity);
+
+        public Task<bool> DeleteAsync(T entity);
+    }
+}
---
+++ src/ParkingZoneMinimalApi/Services/Service.cs
@@ -0,0 +1,10 @@
+﻿using ParkingZoneMinimalApi.Repository.Interfaces;
+using ParkingZoneMinimalApi.Services.Interfaces;
+
+namespace ParkingZoneMinimalApi.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IRepository<T> _repository;
+    }
+}
