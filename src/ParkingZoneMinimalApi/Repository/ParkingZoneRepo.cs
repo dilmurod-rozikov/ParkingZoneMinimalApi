de7ade424e3134906f7569d04633e7d03f801f6a
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-08-31 10:29:50 +0500

    implement the ParkingZone Repo

--- src/ParkingZoneMinimalApi/Repository/ParkingZoneRepo.cs
+++ src/ParkingZoneMinimalApi/Repository/ParkingZoneRepo.cs
@@ -1,6 +1,15 @@
-﻿namespace ParkingZoneMinimalApi.Repository
+﻿using ParkingZoneMinimalApi.DataAccess;
+using ParkingZoneMinimalApi.Models;
+using ParkingZoneMinimalApi.Repository.Interfaces;
+
+namespace ParkingZoneMinimalApi.Repository
 {
-    public class ParkingZoneRepo
+    public class ParkingZoneRepo : Repository<ParkingZone>, IParkingZoneRepo
     {
+        private readonly ApplicationDbContext _context;
+        public ParkingZoneRepo(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
     }
 }
