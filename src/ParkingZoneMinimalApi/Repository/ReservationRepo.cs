24932f27faf41da31e2291efc14262082066a451
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-08-31 10:26:49 +0500

    implement the reservation Repo

--- src/ParkingZoneMinimalApi/Repository/ReservationRepo.cs
+++ src/ParkingZoneMinimalApi/Repository/ReservationRepo.cs
@@ -1,6 +1,15 @@
-﻿namespace ParkingZoneMinimalApi.Repository
+﻿using ParkingZoneMinimalApi.DataAccess;
+using ParkingZoneMinimalApi.Models;
+using ParkingZoneMinimalApi.Repository.Interfaces;
+
+namespace ParkingZoneMinimalApi.Repository
 {
-    public class ReservationRepo
+    public class ReservationRepo : Repository<Reservation>, IReservationRepo
     {
+        private readonly ApplicationDbContext _context;
+        public ReservationRepo(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
     }
 }
