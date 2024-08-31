742cc0c9abb48b6d50ffd22281a8e024f1ab7e9b
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-08-31 10:35:25 +0500

    wire the autoMapper

---
+++ src/ParkingZoneMinimalApi/Helper/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ParkingZoneMinimalApi.DTOs;
+using ParkingZoneMinimalApi.Models;
+
+namespace ParkingZoneMinimalApi.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<ParkingZone, ParkingZoneDto>();
+            CreateMap<ParkingZoneDto, ParkingZone>();
+            CreateMap<ParkingSlot, ParkingSlotDto>();
+            CreateMap<ParkingSlotDto, ParkingSlot>();
+            CreateMap<Reservation, ReservationDto>();
+            CreateMap<ReservationDto, Reservation>();
+        }
+    }
+}
